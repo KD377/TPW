6cc0279b78c30e1924a9e9b093d09004ed23643b
KD377 <242377@edu.p.lodz.pl>
2023-04-12 02:33:01 +0200

    BallAPI bez poruszania sie kul oraz klasa Timer

---
+++ Projekt/Projekt/BallsAPI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic
+{
+    public class BallsAPI
+    {
+        private readonly TimerApi Timer;
+        private List<Ball> Balls;
+        private readonly int BoardWidth;
+        private readonly int BoardHeight;
+        public BallsAPI(int widht, int height)
+        {
+            Balls = new List<Ball>();
+            Timer = TimerApi.CreateBallTimer();
+            this.BoardWidth = widht;
+            this.BoardHeight = height;
+
+        }
+
+        public void CreateBalls(int counter)
+        {
+
+            if(counter >= 0)
+            {
+                int radius = 20;
+                Random random = new Random();
+                for (int i = 0; i < counter; i++)
+                {
+                    int x = random.Next(radius, BoardWidth - radius);
+                    int y = random.Next(radius, BoardHeight - radius);
+                    Ball ball = new Ball(x, y, radius);
+                    Balls.Add(ball);
+
+                }
+            }
+        }
+
+        public int GetX(int index)
+        {
+            if(index >= 0 && index < Balls.Count)
+            {
+                return Balls[index].X;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int GetY(int index)
+        {
+            if (index >= 0 && index < Balls.Count)
+            {
+                return Balls[index].Y;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public int GetBallsNumber()
+        {
+            return Balls.Count;
+        }
+
+        public void Start()
+        {
+            Timer.Start();
+        }
+
+        public void Stop()
+        {
+            Timer.Stop();
+        }
+
+        public void setPeriod(int ms_period)
+        {
+            Timer.Interval = TimeSpan.FromMilliseconds(ms_period);
+        }
+
+
+
+    }
+}
---
+++ Projekt/Projekt/Timer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+
+namespace Logic
+{
+    public abstract class TimerApi
+    {
+        public abstract event EventHandler Tick;
+        public abstract TimeSpan Interval { get; set; }
+        public abstract void Start();
+        public abstract void Stop();
+        public static TimerApi CreateBallTimer()
+        {
+            return new BallTimer();
+        }
+    }
+
+    internal class BallTimer : TimerApi
+    {
+        private Thread timerThread;
+        private bool isRunning;
+
+        public BallTimer()
+        {
+            timerThread = new Thread(new ThreadStart(OnTick));
+            isRunning = false;
+        }
+
+        private void OnTick()
+        {
+            while (isRunning)
+            {
+                Tick?.Invoke(this, EventArgs.Empty);
+                Thread.Sleep(Interval);
+            }
+        }
+
+        public override TimeSpan Interval { get; set; }
+
+        public override event EventHandler Tick;
+
+        public override void Start()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                timerThread.Start();
+            }
+        }
+
+        public override void Stop()
+        {
+            if (isRunning)
+            {
+                isRunning = false;
+                timerThread.Join();
+            }
+        }
+    }
+}

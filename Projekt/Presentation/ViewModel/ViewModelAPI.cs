db884b40b6e3174e4def5cfe9db467e39e102755
242484 Olejniczak Jakub <242484@edu.p.lodz.pl>
2023-04-16 09:47:23 +0200

    Dodanie RelayCommand i ViewModelAPI do ViewModel

---
+++ Projekt/Presentation/ViewModel/ViewModelAPI.cs
@@ -0,0 +1,64 @@
+﻿using Logic;
+using Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class ViewModelAPI : INotifyPropertyChanged
+    {
+        private readonly ModelAPI _model;
+        private ObservableCollection<Ball> _balls;
+        public ICommand StartCommand { get; }
+        public ICommand StopCommand { get; }
+        public ICommand CreateBallCommand { get; }
+
+        public ViewModelAPI(ModelAPI model)
+        {
+            _model = model;
+            StartCommand = new RelayCommand(Start);
+            StopCommand = new RelayCommand(Stop);
+            CreateBallCommand = new RelayCommand(CreateBall);
+            Balls = GetBalls();
+        }
+
+        public ObservableCollection<Ball> Balls
+        {
+            get => _balls;
+            set
+            {
+                _balls = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void Start()
+        {
+            _model.Start();
+        }
+
+        public void Stop()
+        {
+            _model.Stop();
+        }
+
+        public void CreateBall()
+        {
+            _model.CreateBall();
+            Balls = GetBalls();
+        }
+
+        public ObservableCollection<Ball> GetBalls()
+        {
+            return _model.GetBalls();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

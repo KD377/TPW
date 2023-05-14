bd6652bdcce728341103ee93712adce825ca62d3
KD377 <242377@edu.p.lodz.pl>
2023-05-14 14:44:42 +0200

    merge

--- Projekt/Data/Data/DataAPI.cs
+++ Projekt/Data/Data/DataAPI.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,13 +10,62 @@ namespace Data
 {
     public abstract class DataAPI
     {
-        public static DataAPI CreateDataAPI()
+        public abstract BallAPI createBall(CancellationTokenSource cancellationTokenSource);
+        public abstract int getBoardWidth();
+        public abstract int getBoardHeight();
+        public static DataAPI CreateDataAPI(int boardWidth,int boardHeight)
         {
-            return new Data();
+            return new Data(boardWidth,boardHeight);
         }
+
+
     }
     internal class Data : DataAPI
     {
+        private int _boardWidth;
+        private int _boardHeight;
+
+        public Data(int boardWidth,int boardHeight) 
+        {
+            _boardWidth = boardWidth;
+            _boardHeight = boardHeight;
+        }
+        public override int getBoardWidth()
+        {
+            return _boardWidth;
+        }
+
+        public override int getBoardHeight()
+        {
+            return _boardHeight;
+        }
+
+        public override BallAPI createBall(CancellationTokenSource cancellationTokenSource)
+        {
+            Random random = new Random();
+            int x = random.Next(20, _boardWidth - 20);
+            int y = random.Next(20, _boardHeight - 20);
+            int valueX = random.Next(-3, 4);
+            int valueY = random.Next(-3, 4);
+            Vector2 position = new Vector2((int)x, (int)y);
+            Debug.WriteLine(position);
+
+            if (valueX == 0)
+            {
+                valueX = random.Next(1, 3) * 2 - 3;
+            }
+            if (valueY == 0)
+            {
+                valueY = random.Next(1, 3) * 2 - 3;
+            }
+
+            int Vx = valueX;
+            int Vy = valueY;
+            int radius = 20;
+            int mass = 200;
+            return BallAPI.CreateBallAPI(position, Vx, Vy, radius, mass, cancellationTokenSource);
+        }
+
 
     }
 }
